b8ef58b11972aae1faa36375a170a304dbe74d82
juanB <juancruzberrios73@gmail.com>
2024-03-12 18:48:05 -0300

    useContext practice complete

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContex";
+
 export  const HomePage = () => {
+
+  const {user} = useContext(UserContext);
+
     return (
       <>
-        <h1>Home Page</h1>
+        <h1><small>{user?.name}</small>'s Home Page</h1><hr />
+        <pre>
+          {JSON.stringify(user,null,3)}
+        </pre>
       </>
     )
   }
--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,7 +1,22 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContex";
+
 export  const LoginPage = () => {
+
+    const {user,setUser}=useContext(UserContext);
+    console.log(user)
+
     return (
       <>
-        <h1>Login page</h1>
+        <h1>Login page</h1><hr />
+
+        <pre>
+          {JSON.stringify(user,null,3)}
+        </pre>
+
+        <button className="btn btn-primary" onClick={()=>setUser({id: 123,name:"elliot",email:"elliotsepiol@gmail.com"})}>
+          Establecer usuario
+        </button>
       </>
     )
   }
