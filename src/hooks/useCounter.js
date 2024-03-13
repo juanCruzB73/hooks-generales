89a39f23098d2d52361bdfbbf69c008d5d88998c
juanB <juancruzberrios73@gmail.com>
2024-03-12 23:04:18 -0300

    testin hooks

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export const useCounter = (initialValue)=>{
+export const useCounter = (initialValue=10)=>{
     
     const [counter,setCounter]=useState(initialValue)
 
---
+++ tests/hooks/useCounter.test.js
@@ -0,0 +1,33 @@
+import {useCounter} from "../../src/hooks/useCounter"
+const {renderHook,act} = require("@testing-library/react")
+
+describe('useCounter test', () => {
+  
+    test("return default values",()=>{
+        const {result}=renderHook(()=>useCounter());
+        const {counter,decrement,reset,increment}=result.current;
+
+        expect(counter).toBe(10);
+        expect(decrement).toEqual(expect.any(Function));
+        expect(increment).toEqual(expect.any(Function));
+        expect(reset).toEqual(expect.any(Function));
+    });
+
+    test("debe de retornar 100",()=>{
+        const {result}=renderHook(()=>useCounter(100));
+        const {counter}=result.current;
+        expect(counter).toBe(100)
+    });
+
+    test("debe incrementar el contador",()=>{
+        const{result}=renderHook(()=>useCounter());
+        const {counter,increment}=result.current;
+
+        act(()=>{
+            increment();
+        })
+
+        expect(result.current.counter).toBe(11)
+
+    })
+})
